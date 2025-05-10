17ba72fc62072a3fa591f5c96d7b2cd568a17f01
lalit-viyyapu <viyyapulalit@gmail.com>
2025-05-10 14:18:38 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from './App.tsx'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import './App.css'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
